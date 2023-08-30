c73ff822535ceee14987f89a497b8478d24c6980
Rich627 <rich.liu0912@hotmail.com>
2023-08-30 20:23:22 +0800

    update

--- static/portfolio.js
+++ static/portfolio.js
@@ -8,8 +8,6 @@ $(document).ready(function () {
             $('.filter').show('1000');
         }
         else {
-            //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-            //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
             $(".filter").not('.' + value).hide('3000');
             $('.filter').filter('.' + value).show('3000');
         }
